8afc1c456bfd3dd10a512cfa118b4e26600adc56
David Pitchford <david.t.pitchford@gmail.com>
2017-04-18 22:26:27 -0500

    Greatly expanded MLFramework, broke loadData out of the class and implemented crossValidate()

--- Homework/MLFramework.py
+++ Homework/MLFramework.py
@@ -4,76 +4,144 @@ from Feature import Feature
 from sklearn import tree
 import pydotplus
 
+def loadData(inputItr, features, label=None, rowLimit=None):
+    if rowLimit:
+        inputItr = itertools.islice(inputItr, rowLimit)
+    else:
+        inputItr = inputItr
+
+    rows = list(inputItr)
+    numRows = len(rows)
+    data = np.empty((numRows, len(features)), dtype=np.float_)
+    if label is not None:
+        labelData = np.empty((len(rows),), dtype=np.float_)
+
+    for i, row in enumerate(rows):
+        if not row:
+            continue
+        for j, feature in enumerate(features):
+            data[i][j] = feature.extractFeature(row)
+
+        if label is not None:
+            labelData[i] = label.extractFeature(row)
+
+    if label is not None:
+        return data, labelData
+    else:
+        return data
+
+def crossValidate(models, data, labels, k):
+    partitions = np.arange(data.shape[0]) % k
+    np.random.shuffle(partitions)
+    masks = dict()
+    for partition in xrange(k):
+        smallMask = (partitions == partition).nonzero()
+        bigMask = (partitions != partition).nonzero()
+        masks[partition] = (smallMask, bigMask)
+
+    maxFitness, result = 0, None
+    avFitnesses = np.empty((len(models),), dtype=np.float_)
+    for i, model in enumerate(models):
+        fitnesses = np.empty((k,), dtype=np.float_)
+        for partition in xrange(k):
+            smallMask, bigMask = masks[partition]
+
+            trainingData = data[bigMask]
+            testData = data[smallMask]
+            trainingLabels = labels[bigMask]
+            testLabels = labels[smallMask]
+
+            model.fit(trainingData, trainingLabels)
+            fitness = model.testModel(testData, testLabels)
+            fitnesses[partition] = fitness
+        avFitness = np.mean(fitnesses)
+        avFitnesses[i] = avFitness
+        # print "%s: %.4f" % (model, avFitness)
+        if avFitness > maxFitness:
+            maxFitness = avFitness
+            result = model
+    fitnessMean = np.mean(avFitnesses)
+    fitnessStd = np.std(avFitnesses)
+    # print fitnessMean, fitnessStd, maxFitness
+    normalizedFitness = (maxFitness - fitnessMean) / fitnessStd
+
+    return result, normalizedFitness
+
 class SupervisedML(object):
     def __init__(self, features, label, *args, **kwargs):
         self.features = features
         self.label = label
 
-        self.setupModel(*args, **kwargs)
-
-    def loadTrainingData(self, inputItr, rowLimit=None):
-        return self.loadData(inputItr, True, rowLimit)
-
-    def loadTestData(self, inputItr, rowLimit=None):
-        return self.loadData(inputItr, False, rowLimit)
-
-    def loadData(self, inputItr, withLabels, rowLimit):
-        if rowLimit:
-            self.inputItr = itertools.islice(inputItr, rowLimit)
+        self.model = self.makeModel(*args, **kwargs)
+        if args:
+            if kwargs:
+                self.setupArgs = (args, kwargs)
+            else:
+                self.setupArgs = args
+        elif kwargs:
+            self.setupArgs = kwargs
         else:
-            self.inputItr = inputItr
+            self.setupArgs = None
 
-        rows = list(self.inputItr)
-        numRows = len(rows)
-        data = np.empty((numRows, len(self.features)), dtype=np.float_)
-        if withLabels:
-            labelData = np.empty((len(rows),), dtype=np.float_)
+    @classmethod
+    def makeModels(cls, paramRanges, *args, **kwargs):
+        models = list()
 
-        for i, row in enumerate(rows):
-            if not row:
-                continue
-            for j, feature in enumerate(self.features):
-                data[i][j] = feature.extractFeature(row)
+        params = paramRanges.keys()
+        ranges = [paramRanges[param] for param in params]
 
-            if withLabels:
-                labelData[i] = self.label.extractFeature(row)
+        for paramValues in itertools.product(*ranges):
+            paramsDict = dict([(param, v) for param, v in zip(params, paramValues)])
+            kwargs.update(paramsDict)
+            models.append(cls(*args, **kwargs))
 
-        if withLabels:
-            return data, labelData
-        else:
-            return data
+        return models
 
-    def setupModel(self, *args, **kwargs):
-        raise NotImplementedError
-
-    def fit(self, data, labels):
+    @classmethod
+    def makeModel(self, *args, **kwargs):
         raise NotImplementedError
 
     def runModel(self, data):
         raise NotImplementedError
 
+    def __str__(self):
+        if self.setupArgs:
+            return "%s(%s)" % (self.__class__.__name__, str(self.setupArgs))
+        else:
+            return "%s()" % self.__class__.__name__
+
 class Classifier(SupervisedML):
+    def loadTrainingData(self, inputItr, rowLimit=None):
+        return self.loadData(inputItr, self.features, self.label, rowLimit)
+
+    def loadTestData(self, inputItr, rowLimit=None):
+        return self.loadData(inputItr, self.features, self.label, rowLimit)
+
+    def fit(self, data, labels):
+        raise NotImplementedError
+
     def testModel(self, data, labels):
         predictions = self.runModel(data)
 
-        return [p == l for p, l in zip(predictions, labels)].count(True)
+        matched = [p == l for p, l in zip(predictions, labels)].count(True)
+        return float(matched) / data.shape[0]
 
 class DecisionTree(Classifier):
     def __init__(self, features, label, *args, **kwargs):
         super(DecisionTree, self).__init__(features, label, *args, **kwargs)
 
     def exportGraph(self, dest, labelNames=None):
-        dot_data = tree.export_graphviz(self.tree, out_file=None,
+        dot_data = tree.export_graphviz(self.model, out_file=None,
                         feature_names=[f.name for f in self.features],
                         class_names=labelNames, rounded=True, filled=True) 
         graph = pydotplus.graph_from_dot_data(dot_data) 
         graph.write_pdf(dest)
 
-    def setupModel(self, *args, **kwargs):
-        self.tree = tree.DecisionTreeClassifier(*args, **kwargs)
+    def makeModel(self, *args, **kwargs):
+        return tree.DecisionTreeClassifier(*args, **kwargs)
 
     def fit(self, data, labels):
-        self.tree.fit(data, labels)
+        self.model.fit(data, labels)
 
     def runModel(self, data):
-        return self.tree.predict(data)
+        return self.model.predict(data)
--- Homework/Titanic/titanic.py
+++ Homework/Titanic/titanic.py
@@ -3,13 +3,14 @@ import numpy as np
 import csv
 import pydotplus
 import operator
+import itertools
 
 import sys
 import os
 sys.path.append(os.getcwd())
 
 from Feature import LookupFeature
-from MLFramework import DecisionTree
+from MLFramework import DecisionTree, loadData, crossValidate
 
 genderMap = {"male": 1, "female": 2}
 
@@ -24,13 +25,22 @@ label = LookupFeature("Survived")
 
 def main():
     with open("Titanic/train.csv") as fObj:
-        tree = DecisionTree(features, label, max_leaf_nodes=20)
-        trainingData, labels = tree.loadTrainingData(csv.DictReader(fObj))
-        tree.fit(trainingData, labels)
-        # tree.exportGraph("Titanic.pdf", ["Dead", "Survived"])
+        paramRanges = {"criterion": ["gini", "entropy"],
+                       "max_depth": [5, 10, None],
+                       "max_leaf_nodes": [20, None]}
 
-        matched = tree.testModel(trainingData, labels)
-        print "Matched %d / %d" % (matched, labels.shape[0])
+        models = DecisionTree.makeModels(paramRanges, features, label)
+        trainingData, labels = loadData(csv.DictReader(fObj), features, label)
+        bestModel, normalized = crossValidate(models, trainingData, labels, 10)
+        print bestModel, normalized
+
+        # tree = DecisionTree(features, label, max_leaf_nodes=20)
+        # trainingData, labels = tree.loadTrainingData(csv.DictReader(fObj))
+        # tree.fit(trainingData, labels)
+        # # tree.exportGraph("Titanic.pdf", ["Dead", "Survived"])
+
+        # matched = tree.testModel(trainingData, labels)
+        # print "Matched %d / %d" % (matched, labels.shape[0])
 
 if __name__ == "__main__":
     main()
